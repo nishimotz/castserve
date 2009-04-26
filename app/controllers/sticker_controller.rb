b368924cfdf7a0acb622814500cac93a4c98fe58
Takuya Nishimoto <nishimotz@gmail.com>
2009-04-26 19:26:17 +0900

    sticker

--- app/controllers/sticker_controller.rb
+++ app/controllers/sticker_controller.rb
@@ -1,2 +1,19 @@
 class StickerController < ApplicationController
+  def index
+    num = '77735' # default
+    ch = Channel.find_by_number(num)
+    @title = ch.title
+    #@link = 'http://localhost:3000/caststudio/rpc'
+    @description = 'CastStudio'
+    @language = 'ja'
+    @pubdate = Time.parse(Time.new.to_s).rfc822
+    @ch_category = 'CastStudio'
+    @ttl = 90
+    
+    @items = Mediaitem.find_all_by_station(num)
+  end
+  
+  def show
+    @item = Mediaitem.find(params[:id])
+  end
 end
