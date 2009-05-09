c1e2af45a41c00f573cf14be803b438beef0b3c7
Takuya Nishimoto <nishimotz@gmail.com>
2009-05-09 20:17:27 +0900

    snapshot

--- app/controllers/mediaitem_controller.rb
+++ app/controllers/mediaitem_controller.rb
@@ -4,11 +4,14 @@ require 'time'
 class MediaitemController < ApplicationController
   #include WaveUtils
   
+  # from Web Browser : http://ubuntu-vm:3000/mediaitem/
+  # from CastStudio  : http://ubuntu-vm:3000/mediaitem/index.rss?station=77735&uid=101
   def index
     if params[:station]
       num = params[:station]
     else 
-      num = '77735' # default
+      #num = @current_station # '77735' # default
+      num = '77735' 
     end
     #if params[:uid]
     #  uid = params[:uid]
@@ -28,28 +31,31 @@ class MediaitemController < ApplicationController
     @items = Mediaitem.find(:all, :conditions => {:station => num })
     # , :item_type => 'message'
 
-    # for radio_button_tag
-    @episodes = Episode.find(:all, :conditions => {:station=>num})
-    @episodes.each_with_index do |i, idx|
-      def i.selected=(b)
-        @selected = b
+    if params[:format] != 'rss'
+      # for radio_button_tag
+      @episodes = Episode.find(:all, :conditions => {:station=>num})
+      @episodes.each_with_index do |i, idx|
+        def i.selected=(b)
+          @selected = b
+        end
+        def i.selected?
+          @selected
+        end
+        i.selected = false
+        i.selected = true if idx == (@episodes.length - 1)
       end
-      def i.selected?
-        @selected
+      # for check_box_tag
+      @target = {}
+      @items.each do |i|
+        @target[i.id] = false
       end
-      i.selected = false
-      i.selected = true if idx == (@episodes.length - 1)
-    end
-
-    # for check_box_tag
-    @target = {}
-    @items.each do |i|
-      @target[i.id] = false
     end
   end
   
   def new
-    # 
+    @mediaitem = Mediaitem.new
+    @mediaitem.station = @current_station
+    @mediaitem.category = 'message'
   end
   
   def create
